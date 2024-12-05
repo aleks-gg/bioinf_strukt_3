062abd806d0478759ef68345a7644a4534a6a483
a.gostynskiglina <a.gostynskiglina@gmail.com>
2024-12-05 17:42:49 +0100

    fix

--- create_templates.py
+++ create_templates.py
@@ -1,7 +1,22 @@
 from pathlib import Path
 from get_structure import get_structure
 import argparse
+from Bio.PDB import *
 
+
+def save_residue_as_pdb(residue, output_path):
+    structure = Structure.Structure('template')
+    model = Model.Model(0)
+    chain = Chain.Chain('A')
+    
+    new_residue = residue.copy()
+    chain.add(new_residue)
+    model.add(chain)
+    structure.add(model)
+    
+    io = PDBIO()
+    io.set_structure(structure)
+    io.save(str(output_path))
 
 def create_rna_templates(structure_pdb_id, output_dir):
     structure = get_structure(structure_pdb_id)
@@ -28,6 +43,16 @@ def create_rna_templates(structure_pdb_id, output_dir):
                     atom_names = [atom.get_name() for atom in res.get_atoms()]
                     if 'P' in atom_names and 'C4\'' in atom_names:
                         C = res
+
+    output_dir = Path(output_dir)
+    if A:
+        save_residue_as_pdb(A, output_dir / 'A.pdb')
+    if U:
+        save_residue_as_pdb(U, output_dir / 'U.pdb')
+    if G:
+        save_residue_as_pdb(G, output_dir / 'G.pdb')
+    if C:
+        save_residue_as_pdb(C, output_dir / 'C.pdb')
                         
 
 def main():
